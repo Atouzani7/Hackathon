019ba8e24376e0b6a47d1c4ec412e090fc7c66c8
Amajoud Nédim <113235482+SOA-P@users.noreply.github.com>
2022-11-24 21:13:30 +0100

    Revert "Card pays"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import Navbar from "@components/Navbar/navbar";
 import React from "react";
-import CardPays from "@components/CardPays/CardPays";
 
 function App() {
   return (
     <div className="App">
-      <CardPays />
-      <Navbar />
+      
     </div>
   );
 }
