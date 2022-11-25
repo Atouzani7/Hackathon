3bb6c758e7249ac105c443585242610dbbb44f76
Romain HADDAD <romain.haddad@gmail.com>
2022-11-25 11:07:42 +0100

    Adding cards, props & propsTypes

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import raw from "../../mealdb.json";
+import CardMeal from "../CardMeal/CardMeal";
 
 export default function Navbar() {
   const [filteredMeals, setFilteredMeals] = useState(raw.meals);
@@ -15,7 +16,7 @@ export default function Navbar() {
   };
 
   return (
-    <form>
+    <form onSubmit={resultSearch}>
       <label>
         <input
           type="search"
@@ -28,12 +29,10 @@ export default function Navbar() {
           }}
         />
       </label>
-      <input type="submit" value="Rechercher" onClick={resultSearch} />
-      <ul>
-        {filteredMeals.map((meal) => {
-          return <li key={meal.strMeal}>{meal.strMeal}</li>;
-        })}
-      </ul>
+      <input type="submit" value="Rechercher" />
+      {filteredMeals.map((filteredMeal) => {
+        return <CardMeal key={filteredMeal.idMeal} mealData={filteredMeal} />;
+      })}
     </form>
   );
 }
